69b3a6c4e14e80de002b81df8c01950ba26e0b70
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-15 16:21:40 +0530

    Update categoryModel.js

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,35 +1,74 @@
+// // const mongoose = require("mongoose");
+
+// // const categorySchema = new mongoose.Schema(
+// //   {
+// //     name: {
+// //       type: String,
+// //       required: true,
+// //       unique: true,
+// //       trim: true,
+// //     },
+// //   },
+// //   { timestamps: true }
+// // );
+
+// // module.exports = mongoose.model("Category", categorySchema);
+
+// // const mongoose = require("mongoose");
+
+// // const categorySchema = new mongoose.Schema(
+// //   {
+// //     name: {
+// //       type: String,
+// //       required: true,
+// //       unique: true,
+// //       trim: true,
+// //     },
+// //   },
+// //   { timestamps: true }
+// // );
+
+// // module.exports = mongoose.model("Category", categorySchema);
 // const mongoose = require("mongoose");
 
-// const categorySchema = new mongoose.Schema(
+// const CategorySchema = new mongoose.Schema(
 //   {
 //     name: {
 //       type: String,
 //       required: true,
-//       unique: true,
 //       trim: true,
+//     },
+//     image: {
+//       type: String, // cloudinary URL
+//       default: null,
 //     },
 //   },
 //   { timestamps: true }
 // );
 
-// module.exports = mongoose.model("Category", categorySchema);
+// module.exports = mongoose.model("Category", CategorySchema);
 
-// const mongoose = require("mongoose");
 
-// const categorySchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       trim: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Category", categorySchema);
 const mongoose = require("mongoose");
+
+const SubcategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String, // cloudinary URL
+      default: null,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const CategorySchema = new mongoose.Schema(
   {
@@ -42,8 +81,10 @@ const CategorySchema = new mongoose.Schema(
       type: String, // cloudinary URL
       default: null,
     },
+    subcategories: [SubcategorySchema],
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("Category", CategorySchema);
+

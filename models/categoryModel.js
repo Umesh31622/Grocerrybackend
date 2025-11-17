056f2e4871bad346e1d6d097056d749df7831b13
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-17 18:10:19 +0530

    Update categoryModel.js

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,35 +1,23 @@
-// // const mongoose = require("mongoose");
-
-// // const categorySchema = new mongoose.Schema(
-// //   {
-// //     name: {
-// //       type: String,
-// //       required: true,
-// //       unique: true,
-// //       trim: true,
-// //     },
-// //   },
-// //   { timestamps: true }
-// // );
-
-// // module.exports = mongoose.model("Category", categorySchema);
-
-// // const mongoose = require("mongoose");
-
-// // const categorySchema = new mongoose.Schema(
-// //   {
-// //     name: {
-// //       type: String,
-// //       required: true,
-// //       unique: true,
-// //       trim: true,
-// //     },
-// //   },
-// //   { timestamps: true }
-// // );
-
-// // module.exports = mongoose.model("Category", categorySchema);
 // const mongoose = require("mongoose");
+
+// const SubcategorySchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       trim: true,
+//     },
+//     image: {
+//       type: String, // cloudinary URL
+//       default: null,
+//     },
+//     active: {
+//       type: Boolean,
+//       default: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
 // const CategorySchema = new mongoose.Schema(
 //   {
@@ -42,12 +30,12 @@
 //       type: String, // cloudinary URL
 //       default: null,
 //     },
+//     subcategories: [SubcategorySchema],
 //   },
 //   { timestamps: true }
 // );
 
 // module.exports = mongoose.model("Category", CategorySchema);
-
 
 const mongoose = require("mongoose");
 
@@ -87,4 +75,5 @@ const CategorySchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("Category", CategorySchema);
+
 

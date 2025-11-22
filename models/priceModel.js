444455c8454d861bdb5ff79f2587fd6ae49ec2b7
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-22 16:06:50 +0530

    Update priceModel.js

--- models/priceModel.js
+++ models/priceModel.js
@@ -1,3 +1,54 @@
+// const mongoose = require("mongoose");
+
+// const SubRefSchema = new mongoose.Schema(
+//   {
+//     id: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       default: null,
+//     },
+//     name: {
+//       type: String,
+//       default: "",
+//     },
+//     image: {
+//       type: String,
+//       default: null,
+//     },
+//   },
+//   { _id: false }
+// );
+
+// const priceSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+
+//   category: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "Category",
+//     required: true,
+//   },
+
+//   // embedded snapshot of subcategory (not populated via mongoose)
+//   subcategory: {
+//     type: SubRefSchema,
+//     default: null,
+//   },
+
+//   basePrice: { type: Number, required: true },
+//   difference: { type: Number, default: 0 },
+//   validTill: { type: Date },
+//   description: { type: String },
+//   image: { type: String },
+
+//   status: {
+//     type: String,
+//     enum: ["active", "inactive"],
+//     default: "active",
+//   },
+
+//   createdAt: { type: Date, default: Date.now },
+// });
+
+// module.exports = mongoose.models.Price || mongoose.model("Price", priceSchema);
 
 const mongoose = require("mongoose");
 

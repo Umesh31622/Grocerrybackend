bec869ad6055fb08a231d04f563488ad5033c606
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-22 14:46:45 +0530

    Update priceRoutes.js

--- routes/priceRoutes.js
+++ routes/priceRoutes.js
@@ -82,46 +82,6 @@
 
 
 
-// const express = require("express");
-// const router = express.Router();
-// const multer = require("multer");
-
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
-
-// const priceController = require("../controllers/priceController");
-
-// // Website API
-// router.get("/website", priceController.getWebsitePrices);
-
-// // Status update
-// router.put("/status/:id", priceController.updateStatus);
-
-// // Bulk update
-// router.post("/bulk-update", priceController.bulkUpdatePrices);
-
-// // Copy product
-// router.post("/copy/:id", priceController.copyPrice);
-
-// // Get all products
-// router.get("/", priceController.getPrices);
-
-// // Create
-// router.post("/", upload.single("file"), priceController.createPrice);
-
-// // Import CSV
-// router.post("/import", upload.single("file"), priceController.importPrices);
-
-// // Export CSV
-// router.get("/export", priceController.exportPrices);
-
-// // Update
-// router.put("/:id", upload.single("file"), priceController.updatePrice);
-
-// // Delete
-// router.delete("/:id", priceController.deletePrice);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
@@ -137,16 +97,13 @@ router.get("/website", priceController.getWebsitePrices);
 // Status update
 router.put("/status/:id", priceController.updateStatus);
 
-// MAIN DIFF LOGIC
-router.put("/diff/:id", priceController.updateDiff);
-
 // Bulk update
 router.post("/bulk-update", priceController.bulkUpdatePrices);
 
-// Copy
+// Copy product
 router.post("/copy/:id", priceController.copyPrice);
 
-// All
+// Get all products
 router.get("/", priceController.getPrices);
 
 // Create
@@ -158,14 +115,10 @@ router.post("/import", upload.single("file"), priceController.importPrices);
 // Export CSV
 router.get("/export", priceController.exportPrices);
 
-// Update Meta (NO diff)
+// Update
 router.put("/:id", upload.single("file"), priceController.updatePrice);
 
 // Delete
 router.delete("/:id", priceController.deletePrice);
 
 module.exports = router;
-
-
-
-

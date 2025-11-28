1cf1d3fb9cc14e957a1bf2f01fcdb4d723689845
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-28 15:24:57 +0530

    Refactor priceRoutes.js with new endpoints

--- routes/priceRoutes.js
+++ routes/priceRoutes.js
@@ -1,3 +1,44 @@
+// // // // // // // const express = require("express");
+// // // // // // // const router = express.Router();
+// // // // // // // const multer = require("multer");
+
+// // // // // // // const storage = multer.memoryStorage();
+// // // // // // // const upload = multer({ storage });
+
+// // // // // // // const priceController = require("../controllers/priceController");
+
+// // // // // // // // Website API
+// // // // // // // router.get("/website", priceController.getWebsitePrices);
+
+// // // // // // // // Status update
+// // // // // // // router.put("/status/:id", priceController.updateStatus);
+
+// // // // // // // // Bulk update
+// // // // // // // router.post("/bulk-update", priceController.bulkUpdatePrices);
+
+// // // // // // // // Copy product
+// // // // // // // router.post("/copy/:id", priceController.copyPrice);
+
+// // // // // // // // Get all products
+// // // // // // // router.get("/", priceController.getPrices);
+
+// // // // // // // // Create
+// // // // // // // router.post("/", upload.single("file"), priceController.createPrice);
+
+// // // // // // // // Import CSV
+// // // // // // // router.post("/import", upload.single("file"), priceController.importPrices);
+
+// // // // // // // // Export CSV
+// // // // // // // router.get("/export", priceController.exportPrices);
+
+// // // // // // // // Update
+// // // // // // // router.put("/:id", upload.single("file"), priceController.updatePrice);
+
+// // // // // // // // Delete
+// // // // // // // router.delete("/:id", priceController.deletePrice);
+
+// // // // // // // module.exports = router;
+
 // // // // // // const express = require("express");
 // // // // // // const router = express.Router();
 // // // // // // const multer = require("multer");
@@ -37,8 +78,50 @@
 // // // // // // // Delete
 // // // // // // router.delete("/:id", priceController.deletePrice);
 
-// // // // // // module.exports = router;
+// // // // // // // module.exports = router;
+// // // // // // const express = require("express");
+// // // // // // const router = express.Router();
+// // // // // // const multer = require("multer");
 
+// // // // // // const storage = multer.memoryStorage();
+// // // // // // const upload = multer({ storage });
+
+// // // // // // const priceController = require("../controllers/priceController");
+
+// // // // // // // Website API
+// // // // // // router.get("/website", priceController.getWebsitePrices);
+
+// // // // // // // Status update
+// // // // // // router.put("/status/:id", priceController.updateStatus);
+
+// // // // // // // Bulk update
+// // // // // // router.post("/bulk-update", priceController.bulkUpdatePrices);
+
+// // // // // // // Copy product
+// // // // // // router.post("/copy/:id", priceController.copyPrice);
+
+// // // // // // // Update DIFF (VERY IMPORTANT)
+// // // // // // router.put("/updateDiff/:id", priceController.updateDiff);
+
+// // // // // // // Get all products
+// // // // // // router.get("/", priceController.getPrices);
+
+// // // // // // // Create
+// // // // // // router.post("/", upload.single("file"), priceController.createPrice);
+
+// // // // // // // Import CSV
+// // // // // // router.post("/import", upload.single("file"), priceController.importPrices);
+
+// // // // // // // Export CSV
+// // // // // // router.get("/export", priceController.exportPrices);
+
+// // // // // // // Update
+// // // // // // router.put("/:id", upload.single("file"), priceController.updatePrice);
+
+// // // // // // // Delete
+// // // // // // router.delete("/:id", priceController.deletePrice);
+
+// // // // // // module.exports = router;
 // // // // // const express = require("express");
 // // // // // const router = express.Router();
 // // // // // const multer = require("multer");
@@ -48,77 +131,37 @@
 
 // // // // // const priceController = require("../controllers/priceController");
 
-// // // // // // Website API
+// // // // // // WEBSITE API (only active)
 // // // // // router.get("/website", priceController.getWebsitePrices);
 
-// // // // // // Status update
+// // // // // // STATUS update
 // // // // // router.put("/status/:id", priceController.updateStatus);
 
-// // // // // // Bulk update
+// // // // // // BULK update
 // // // // // router.post("/bulk-update", priceController.bulkUpdatePrices);
 
-// // // // // // Copy product
+// // // // // // COPY product
 // // // // // router.post("/copy/:id", priceController.copyPrice);
 
-// // // // // // Get all products
-// // // // // router.get("/", priceController.getPrices);
-
-// // // // // // Create
-// // // // // router.post("/", upload.single("file"), priceController.createPrice);
-
-// // // // // // Import CSV
-// // // // // router.post("/import", upload.single("file"), priceController.importPrices);
-
-// // // // // // Export CSV
-// // // // // router.get("/export", priceController.exportPrices);
-
-// // // // // // Update
-// // // // // router.put("/:id", upload.single("file"), priceController.updatePrice);
-
-// // // // // // Delete
-// // // // // router.delete("/:id", priceController.deletePrice);
-
-// // // // // // module.exports = router;
-// // // // // const express = require("express");
-// // // // // const router = express.Router();
-// // // // // const multer = require("multer");
-
-// // // // // const storage = multer.memoryStorage();
-// // // // // const upload = multer({ storage });
-
-// // // // // const priceController = require("../controllers/priceController");
-
-// // // // // // Website API
-// // // // // router.get("/website", priceController.getWebsitePrices);
-
-// // // // // // Status update
-// // // // // router.put("/status/:id", priceController.updateStatus);
-
-// // // // // // Bulk update
-// // // // // router.post("/bulk-update", priceController.bulkUpdatePrices);
-
-// // // // // // Copy product
-// // // // // router.post("/copy/:id", priceController.copyPrice);
-
-// // // // // // Update DIFF (VERY IMPORTANT)
+// // // // // // UPDATE DIFF (VERY IMPORTANT)
 // // // // // router.put("/updateDiff/:id", priceController.updateDiff);
 
-// // // // // // Get all products
+// // // // // // GET ALL products
 // // // // // router.get("/", priceController.getPrices);
 
-// // // // // // Create
+// // // // // // CREATE price
 // // // // // router.post("/", upload.single("file"), priceController.createPrice);
 
-// // // // // // Import CSV
+// // // // // // CSV IMPORT
 // // // // // router.post("/import", upload.single("file"), priceController.importPrices);
 
-// // // // // // Export CSV
+// // // // // // CSV EXPORT
 // // // // // router.get("/export", priceController.exportPrices);
 
-// // // // // // Update
+// // // // // // UPDATE price
 // // // // // router.put("/:id", upload.single("file"), priceController.updatePrice);
 
-// // // // // // Delete
+// // // // // // DELETE price
 // // // // // router.delete("/:id", priceController.deletePrice);
 
 // // // // // module.exports = router;
@@ -165,6 +208,7 @@
 // // // // router.delete("/:id", priceController.deletePrice);
 
 // // // // module.exports = router;
+
 // // // const express = require("express");
 // // // const router = express.Router();
 // // // const multer = require("multer");
@@ -174,37 +218,19 @@
 
 // // // const priceController = require("../controllers/priceController");
 
-// // // // WEBSITE API (only active)
 // // // router.get("/website", priceController.getWebsitePrices);
-
-// // // // STATUS update
 // // // router.put("/status/:id", priceController.updateStatus);
-
-// // // // BULK update
 // // // router.post("/bulk-update", priceController.bulkUpdatePrices);
-
-// // // // COPY product
 // // // router.post("/copy/:id", priceController.copyPrice);
-
-// // // // UPDATE DIFF (VERY IMPORTANT)
 // // // router.put("/updateDiff/:id", priceController.updateDiff);
 
-// // // // GET ALL products
 // // // router.get("/", priceController.getPrices);
-
-// // // // CREATE price
 // // // router.post("/", upload.single("file"), priceController.createPrice);
 
-// // // // CSV IMPORT
 // // // router.post("/import", upload.single("file"), priceController.importPrices);
-
-// // // // CSV EXPORT
 // // // router.get("/export", priceController.exportPrices);
 
-// // // // UPDATE price
 // // // router.put("/:id", upload.single("file"), priceController.updatePrice);
-
-// // // // DELETE price
 // // // router.delete("/:id", priceController.deletePrice);
 
 // // // module.exports = router;
@@ -244,19 +270,46 @@
 
 // const priceController = require("../controllers/priceController");
 
+// // WEBSITE
 // router.get("/website", priceController.getWebsitePrices);
+
+// // STATUS
 // router.put("/status/:id", priceController.updateStatus);
+
+// // BULK
 // router.post("/bulk-update", priceController.bulkUpdatePrices);
+
+// // COPY
 // router.post("/copy/:id", priceController.copyPrice);
+
+// // QUICK DIFF
 // router.put("/updateDiff/:id", priceController.updateDiff);
 
+// // GET ALL
 // router.get("/", priceController.getPrices);
+
+// // CREATE
 // router.post("/", upload.single("file"), priceController.createPrice);
 
+// // IMPORT CSV
 // router.post("/import", upload.single("file"), priceController.importPrices);
+
+// // EXPORT CSV
 // router.get("/export", priceController.exportPrices);
 
+// // SELECTED EXPORT
+// router.post("/export-selected", priceController.exportSelected);
+
+// // SELECTED DELETE
+// router.post("/delete-selected", priceController.deleteSelected);
+
+// // SELECTED CSV IMPORT
+// router.post("/import-selected", upload.single("file"), priceController.importSelected);
+
+// // UPDATE
 // router.put("/:id", upload.single("file"), priceController.updatePrice);
+
+// // DELETE
 // router.delete("/:id", priceController.deletePrice);
 
 // module.exports = router;
@@ -270,7 +323,7 @@ const upload = multer({ storage });
 
 const priceController = require("../controllers/priceController");
 
-// WEBSITE
+// WEBSITE ACTIVE
 router.get("/website", priceController.getWebsitePrices);
 
 // STATUS
@@ -282,7 +335,7 @@ router.post("/bulk-update", priceController.bulkUpdatePrices);
 // COPY
 router.post("/copy/:id", priceController.copyPrice);
 
-// QUICK DIFF
+// QUICK UPDATE (PROFIT/LOSS ADD)
 router.put("/updateDiff/:id", priceController.updateDiff);
 
 // GET ALL
@@ -291,25 +344,23 @@ router.get("/", priceController.getPrices);
 // CREATE
 router.post("/", upload.single("file"), priceController.createPrice);
 
-// IMPORT CSV
+// CSV IMPORT
 router.post("/import", upload.single("file"), priceController.importPrices);
 
-// EXPORT CSV
+// CSV EXPORT
 router.get("/export", priceController.exportPrices);
-
-// SELECTED EXPORT
-router.post("/export-selected", priceController.exportSelected);
 
 // SELECTED DELETE
 router.post("/delete-selected", priceController.deleteSelected);
 
-// SELECTED CSV IMPORT
-router.post("/import-selected", upload.single("file"), priceController.importSelected);
+// SELECTED EXPORT
+router.post("/export-selected", priceController.exportPrices);
 
-// UPDATE
+// UPDATE SINGLE
 router.put("/:id", upload.single("file"), priceController.updatePrice);
 
 // DELETE
 router.delete("/:id", priceController.deletePrice);
 
 module.exports = router;
+ 

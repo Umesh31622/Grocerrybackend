c93825dbbef805b20e2de189ef2e17895dfb59e5
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-01 01:12:57 +0530

    Update priceRoutes.js

--- routes/priceRoutes.js
+++ routes/priceRoutes.js
@@ -1,59 +1,8 @@
-
-// const express = require("express");
-// const router = express.Router();
-// const multer = require("multer");
-
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
-
-// const priceController = require("../controllers/priceController");
-
-// // WEBSITE ACTIVE PRICES
-// router.get("/website", priceController.getWebsitePrices);
-
-// // STATUS CHANGE
-// router.put("/status/:id", priceController.updateStatus);
-
-// // BULK UPDATE
-// router.post("/bulk-update", priceController.bulkUpdatePrices);
-
-// // COPY PRICE
-// router.post("/copy/:id", priceController.copyPrice);
-
-// // QUICK PROFIT/LOSS UPDATE
-// router.put("/updateDiff/:id", priceController.updateDiff);
-
-// // GET ALL PRICES
-// router.get("/", priceController.getPrices);
-
-// // CREATE PRICE
-// router.post("/", upload.single("file"), priceController.createPrice);
-
-// // IMPORT CSV
-// router.post("/import", upload.single("file"), priceController.importPrices);
-
-// // EXPORT ALL CSV
-// router.get("/export", priceController.exportPrices);
-
-// // DELETE SELECTED
-// router.post("/delete-selected", priceController.deleteSelected);
-
-// // EXPORT SELECTED
-// router.post("/export-selected", priceController.exportSelected);
-
-// // UPDATE PRICE
-// router.put("/:id", upload.single("file"), priceController.updatePrice);
-
-// // DELETE PRICE
-// router.delete("/:id", priceController.deletePrice);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 
 const upload = multer({ storage: multer.memoryStorage() });
-
 const priceController = require("../controllers/priceController");
 
 // WEBSITE ACTIVE PRICES
@@ -83,7 +32,7 @@ router.post("/import", upload.single("file"), priceController.importPrices);
 // EXPORT ALL CSV
 router.get("/export", priceController.exportPrices);
 
-// EXPORT SELECTED
+// EXPORT SELECTED CSV
 router.post("/export-selected", priceController.exportSelected);
 
 // DELETE SELECTED

80e126b3e2f89022afbf884e6a1592686221a96f
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-11-30 23:46:57 +0530

    Uncomment price routes in priceRoutes.js

--- routes/priceRoutes.js
+++ routes/priceRoutes.js
@@ -1,3 +1,52 @@
+// // const express = require("express");
+// // const router = express.Router();
+// // const multer = require("multer");
+
+// // const storage = multer.memoryStorage();
+// // const upload = multer({ storage });
+
+// // const priceController = require("../controllers/priceController");
+
+// // // Website Active
+// // router.get("/website", priceController.getWebsitePrices);
+
+// // // Status Update
+// // router.put("/status/:id", priceController.updateStatus);
+
+// // // Bulk Update
+// // router.post("/bulk-update", priceController.bulkUpdatePrices);
+
+// // // Copy Price
+// // router.post("/copy/:id", priceController.copyPrice);
+
+// // // Quick Profit/Loss Change
+// // router.put("/updateDiff/:id", priceController.updateDiff);
+
+// // // // Import CSV
+// // // router.post("/import", upload.single("file"), priceController.importPrices);
+
+// // // Export ALL CSV
+// // router.get("/export", priceController.exportPrices);
+
+// // // Delete Multiple
+// // router.post("/delete-selected", priceController.deleteSelected);
+
+// // // Export Selected (MUST exist)
+// // router.post("/export-selected", priceController.exportSelected);
+
+// // // Get All Prices
+// // router.get("/", priceController.getPrices);
+
+// // // Create
+// // router.post("/", upload.single("file"), priceController.createPrice);
+
+// // // Update
+// // router.put("/:id", upload.single("file"), priceController.updatePrice);
+
+// // // Delete
+// // router.delete("/:id", priceController.deletePrice);
+
+// // module.exports = router;
 // const express = require("express");
 // const router = express.Router();
 // const multer = require("multer");
@@ -7,46 +56,47 @@
 
 // const priceController = require("../controllers/priceController");
 
-// // Website Active
+// // WEBSITE ACTIVE PRICES
 // router.get("/website", priceController.getWebsitePrices);
 
-// // Status Update
+// // STATUS CHANGE
 // router.put("/status/:id", priceController.updateStatus);
 
-// // Bulk Update
+// // BULK UPDATE
 // router.post("/bulk-update", priceController.bulkUpdatePrices);
 
-// // Copy Price
+// // COPY PRICE
 // router.post("/copy/:id", priceController.copyPrice);
 
-// // Quick Profit/Loss Change
+// // QUICK PROFIT/LOSS UPDATE
 // router.put("/updateDiff/:id", priceController.updateDiff);
 
-// // // Import CSV
-// // router.post("/import", upload.single("file"), priceController.importPrices);
-
-// // Export ALL CSV
-// router.get("/export", priceController.exportPrices);
-
-// // Delete Multiple
-// router.post("/delete-selected", priceController.deleteSelected);
-
-// // Export Selected (MUST exist)
-// router.post("/export-selected", priceController.exportSelected);
-
-// // Get All Prices
+// // GET ALL PRICES
 // router.get("/", priceController.getPrices);
 
-// // Create
+// // CREATE PRICE
 // router.post("/", upload.single("file"), priceController.createPrice);
 
-// // Update
+// // IMPORT CSV
+// router.post("/import", upload.single("file"), priceController.importPrices);
+
+// // EXPORT ALL CSV
+// router.get("/export", priceController.exportPrices);
+
+// // DELETE SELECTED
+// router.post("/delete-selected", priceController.deleteSelected);
+
+// // EXPORT SELECTED
+// router.post("/export-selected", priceController.exportSelected);
+
+// // UPDATE PRICE
 // router.put("/:id", upload.single("file"), priceController.updatePrice);
 
-// // Delete
+// // DELETE PRICE
 // router.delete("/:id", priceController.deletePrice);
 
 // module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");

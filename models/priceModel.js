c4e7e2a26c6ab7e140102c27854b383468d71172
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2025-12-01 01:12:33 +0530

    Update priceModel.js

--- models/priceModel.js
+++ models/priceModel.js
@@ -1,46 +1,3 @@
-
-// const mongoose = require("mongoose");
-
-// const priceSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-
-//   category: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "Category",
-//     required: true,
-//   },
-
-//   subcategory: {
-//     id: String,
-//     name: String,
-//     image: String,
-//   },
-
-//   basePrice: { type: Number, required: true },
-//   profitLoss: { type: Number, default: 0 },
-//   salePrice: { type: Number, default: 0 },
-
-//   lockedPrice: { type: Number, default: 0 },
-//   yesterdayLock: { type: Number, default: 0 },
-//   brokerDisplay: { type: Number, default: 0 },
-
-//   // store last date (YYYY-MM-DD) when lock was applied so we don't double-lock same day
-//   lastLockDate: { type: String, default: "" },
-
-//   validTill: Date,
-//   description: String,
-//   image: String,
-
-//   status: {
-//     type: String,
-//     enum: ["active", "inactive"],
-//     default: "inactive",
-//   },
-
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// module.exports = mongoose.models.Price || mongoose.model("Price", priceSchema);
 const mongoose = require("mongoose");
 
 const priceSchema = new mongoose.Schema({
@@ -78,7 +35,7 @@ const priceSchema = new mongoose.Schema({
     default: "inactive",
   },
 
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Price", priceSchema);

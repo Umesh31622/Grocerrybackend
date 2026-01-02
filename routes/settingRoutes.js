2c867a40063c4350114f5b03d900e75d35087d2a
umesh Kumar <153422845+Umesh31622@users.noreply.github.com>
2026-01-02 19:06:19 +0530

    Refactor settingRoutes.js for cleaner code

--- routes/settingRoutes.js
+++ routes/settingRoutes.js
@@ -1,66 +1,24 @@
-// const express = require("express");
-// const router = express.Router();
-// const Setting = require("../models/Setting");
-
-// // Get Current Status
-// router.get("/", async (req, res) => {
-//   let setting = await Setting.findOne();
-//   if (!setting) setting = await Setting.create({ websiteActive: true });
-
-//   res.json(setting);
-// });
-
-// // Toggle Status
-// router.put("/toggle", async (req, res) => {
-//   let setting = await Setting.findOne();
-//   if (!setting) setting = await Setting.create({ websiteActive: true });
-
-//   setting.websiteActive = !setting.websiteActive;
-//   await setting.save();
-
-//   res.json({ success: true, websiteActive: setting.websiteActive });
-// });
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const Setting = require("../models/Setting");
 
 // Get Current Status
 router.get("/", async (req, res) => {
-  try {
-    let setting = await Setting.findOne();
+  let setting = await Setting.findOne();
+  if (!setting) setting = await Setting.create({ websiteActive: true });
 
-    // ❌ default TRUE hata diya
-    if (!setting) {
-      setting = await Setting.create({ websiteActive: false });
-    }
-
-    res.json(setting);
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
+  res.json(setting);
 });
 
 // Toggle Status
 router.put("/toggle", async (req, res) => {
-  try {
-    let setting = await Setting.findOne();
+  let setting = await Setting.findOne();
+  if (!setting) setting = await Setting.create({ websiteActive: true });
 
-    if (!setting) {
-      setting = await Setting.create({ websiteActive: false });
-    }
+  setting.websiteActive = !setting.websiteActive;
+  await setting.save();
 
-    setting.websiteActive = !setting.websiteActive;
-    await setting.save();
-
-    res.json({ success: true, websiteActive: setting.websiteActive });
-  } catch (err) {
-    res.status(500).json({ success: false, message: err.message });
-  }
+  res.json({ success: true, websiteActive: setting.websiteActive });
 });
 
 module.exports = router;
-
-
